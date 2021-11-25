805c53a49f5cf71cec76ff7eaa83395ff0599eb3
Hansel <hansel.hir@gmail.com>
2021-11-24 20:01:01 -0500

    funcion printf: permite la salida de los datos

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Mostrando la salida de los datos
+int main() {
+    char nombre[10] = "Ada Love";
+    int edad = 39;
+    long km = 1200100300;
+    double precio = 25.3;
+    printf("Hola %s tengo %d y mi auto tiene de km %ld y el precio es %lf!\n",nombre,edad,km,precio);
+    return 0;
+}
